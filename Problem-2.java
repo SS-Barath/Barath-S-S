9413968986ba4653e4c45dccb16190c594eb617e
Barath S S <162808878+SS-Barath@users.noreply.github.com>
2025-08-07 20:08:37 +0530

    Create Problem-2.java

---
+++ Problem-2.java
@@ -0,0 +1,35 @@
+With a single integer as the input, generate the following until a = x [series of numbers as shown in below examples]
+ 
+  Output: (examples)
+    1) input a = 1, then output : 1
+    2) input a = 2, then output : 1, 3
+    3) input a = 3, then output : 1, 3, 5
+    4) input a = 4, then output : 1, 3, 5, 7
+    .
+    .
+    5) input a = x, then output : 1, 3, 5, 7, .......
+
+
+Program:
+
+import java.util.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        
+        int num = 1;
+        
+        for(int i=0;i<n;i++){
+            System.out.print(num);
+            if(i < n - 1){
+                System.out.print(",");
+            }
+            num = num + 2;
+        }
+     sc.close();
+    }
+}
