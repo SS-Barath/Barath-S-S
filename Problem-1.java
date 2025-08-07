b1a152564c5c4c30428295bd07c6c743183e5994
Barath S S <162808878+SS-Barath@users.noreply.github.com>
2025-08-07 20:06:52 +0530

    Update Problem-1.java

--- Problem-1.java
+++ Problem-1.java
@@ -1,6 +1,6 @@
 Create a small calculator which performs operations such as Addition, Subtraction, Multiplication and Division using class.
-  Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
-  Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string
+- Calculator inputs :> ‘a’, ‘b’ and ‘type of operation’
+- Datatype :> ‘a’ = double, ‘b’ = double, ‘type of operation’ = string
 
 
 Program:
